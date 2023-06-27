03201cf4211dfa5058901aff73a6b29db0596056
saint8083 <kvictoria040@gmail.com>
2023-06-27 13:34:35 -0700

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
+#!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for i in my_list:
-            print(i, end=' ')
-            count += 1
-            if count == x:
-                break
-        print()
-        return count
-    except TypeError:
-        print("Error: Invalid list")
-        return count
+    total = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            total += 1
+        except IndexError:
+            break
+    print()
+    return (total)
