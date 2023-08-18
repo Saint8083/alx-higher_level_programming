0ad90e13dab286319f40e481713bf06d887d8b99
saint8083 <kvictoria040@gmail.com>
2023-08-18 05:33:52 -0700

    update SQL

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
--- creates the MySQL server user user_0d_1
--- creates a new user
+-- creates the MySQL server user user_0d_1 and grant all priviledges
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
--- grants privileges
 GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
