f12733c6b3b5693e1c5964600d479cc47eb5048b
saint8083 <kvictoria040@gmail.com>
2023-09-12 07:11:25 -0700

    updated add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-// A script that prints the addition of 2 
-
+const a = process.argv[2];
+const b = process.argv[3];
 function add (a, b) {
-  const c = a + b;
-  console.log(c);
+  if (isNaN(a) || isNaN(b)) {
+    return (NaN);
+  } else {
+    return (parseInt(a) + parseInt(b));
+  }
 }
-
-add(Number(process.argv[2]), Number(process.argv[3]));
+console.log(add(a, b));
